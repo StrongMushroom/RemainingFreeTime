af80618de0f2faf1fe9e21dc74b9b2be786c780d
InSeoKang <inseo10263@gmail.com>
2018-08-04 19:00:01 +0900

    수정 창 색 변경

--- RemainingFreeTime/VC/ModifyCellViewController.swift
+++ RemainingFreeTime/VC/ModifyCellViewController.swift
@@ -183,7 +183,7 @@ class ModifyCellViewController: UIViewController {
         warningLabel.textColor = UIColor.clear
     }
     func changeButtonBackgroundColor(button : UIButton){
-        button.backgroundColor = UIColor(red: 233, green: 201, blue: 137, alpha: 0.9)
+        button.backgroundColor = UIColor(red: 0.91, green: 0.78, blue: 0.54, alpha: 0.9)
     }
     @objc func doneTapped(sender:UIButton) {
         clearLabelColor()
